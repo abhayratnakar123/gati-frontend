584f18fd97e61a9a90c34386e6d1cda40317cd21
Abhay Ratnakar <abhayratnakar03@gmail.com>
2024-07-06 10:16:14 +0530

    First-Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-5xl bg-orange-200 text-center">
+      Hy From Gati-Frontend
+    </div>
+  );
+}
