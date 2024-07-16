adceec914f5258a31b46c75ac9083a22f93e6726
Abhay Ratnakar <abhayratnakar03@gmail.com>
2024-07-17 00:34:43 +0530

    Changes In homePage

--- src/components/prodect/index.tsx
+++ src/components/prodect/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProductCard = ({ icon, title, description }:any) => {
   return (
-    <div className="bg-black h-[60vh] lg:w-[20vw] text-white p-6 rounded-lg shadow-md transform transition-transform hover:scale-105">
+    <div className="bg-[#212121] h-[60vh] lg:w-[20vw] text-white p-6 rounded-lg shadow-md transform transition-transform hover:scale-105">
       <div className="flex justify-center mb-4">
         <div className="bg-gradient-to-r from-purple-500 to-blue-500 rounded-lg">
           <img src='https://img.freepik.com/free-vector/code-typing-concept-illustration_114360-4296.jpg?t=st=1720610612~exp=1720614212~hmac=6eba37c0762460f56d1618f2fd8155c724610981131331ff6207883025a102d3&w=740' alt={`${title} icon`} className="h-40" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,80 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["PT Sans", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
+      animation: {
+        'loop-scroll': 'loop-scroll 50s linear infinite',
+        "fade-out-down": "fade-out-down linear forwards",
+        "make-it-bigger": "make-it-bigger linear forwards",
+        "typing": 'typing 4s steps(40, end) 1s 1 normal both, blink .75s step-end infinite',
+        "fadeInUp": 'fadeInUp 0.5s ease-out forwards',
+      },
+      keyframes: {
+        "fade-out-down": {
+          from: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          to: {
+            opacity: "0",
+            transform: "translateY(40%)",
+          },
+        },
+        "make-it-bigger": {
+          "0%": {
+            transform: "translateY(0%)",
+          },
+          "80%": {
+            transform: "translateY(-30%)",
+          },
+          "90%": {
+            transform: "translateY(-10%) scale(0.75)",
+          },
+          "100%": {
+            transform: "translateY(0%) scale(0.5)",
+            opacity: 0.6,
+            zIndex: -1,
+          },
+        },
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+        typing: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '0%, 100%': { borderColor: 'transparent' },
+          '50%': { borderColor: 'black' },
+        },
+
+        fadeInUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      } ,
+      supports: {
+        "no-scroll-driven-animations": "not(animation-timeline: scroll())",
+      },
+    },    
   },
   plugins: [],
 };
+
+
+
 export default config;
